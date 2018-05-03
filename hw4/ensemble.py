67485a556fb62821288a4d0a71492fe010cb6360
EdwinYam <b03901036@ntu.edu.tw>
2018-05-03 21:03:13 +0800

    ensemble

--- hw4/ensemble.py
+++ hw4/ensemble.py
@@ -3,10 +3,7 @@ from keras import layers
 from keras.models import Model
 from keras.layers import Input
 
-def ensemble_models(models=['my_model_67901.h5','my_model_68013.h5','my_model_67539.h5']):
-    model_collection = []
-    for i in models:
-        model_collection.append(load_model(i))
+def ensemble_models(model_collection):
     model_input = Input(shape=model_collection[0].input_shape[1:])
     pre_avg=[model(model_input) for model in model_collection] 
     Avg=layers.average(pre_avg)
@@ -15,4 +12,8 @@ def ensemble_models(models=['my_model_67901.h5','my_model_68013.h5','my_model_67
     return modelEns
 
 if __name__ == "__main__":
-    ensemble_models()
+    model_collection = []
+    models=['my_model_67901.h5','my_model_68013.h5','my_model_67539.h5']
+    for i in models:
+        model_collection.append(load_model(i))
+    ensemble_models(model_collection)
