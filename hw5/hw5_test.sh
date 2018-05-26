c9e82e3f02b77bedbc235ada0ede41a0ed0ccd85
Jexus <b05901033@ntu.edu.tw>
2018-05-26 20:38:25 +0800

    wget -O

--- hw5/hw5_test.sh
+++ hw5/hw5_test.sh
@@ -1,2 +1,3 @@
-wget https://www.dropbox.com/s/zxgdldl6aiwxekw/ensemble_82966.h5?dl=1
+wget -O ensemble_82966.h5 https://www.dropbox.com/s/zxgdldl6aiwxekw/ensemble_82966.h5?dl=1
 python3 hw5_test.py $1 $2 $3
+rm -rf ensemble_82966.h5*
